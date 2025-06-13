94091d37f36be18b32629b6fc62d685ec3ab054e
Faisal Abusharar <77110462+FaisalAbusharar@users.noreply.github.com>
2025-06-13 18:42:42 +0300

    Published First Version

---
+++ CVDA.py
@@ -0,0 +1,86 @@
+from nextcord.ext import commands
+import nextcord
+import os
+import webbrowser
+
+from pynput.keyboard import Key, Controller
+
+
+
+# DO NOT CHANGE THE FILE NAME.
+
+
+# ------------ VARIABLES YOU CHANGE ---------------
+
+browser_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s'  # Change this based on your browser path.
+discord_token = "MTM4MzA0NzYyNDAyNTcwNjUyNw.GVDa8v.ekXXeuD9FgtpxT0YI4IIqIgCTP7LMSc_-Yf7kc" # Add your discord bot token here.
+prefix = "PC-" # The prefix in which you type the commands.
+
+# ------------------------------------------------
+
+keyboard = Controller()
+
+
+intents = nextcord.Intents.default()
+intents.message_content = True
+
+# the prefix is not used in this example
+bot = commands.Bot(command_prefix=prefix, intents=intents)
+
+
+
+@bot.command()
+async def shutdown(ctx):
+    os.system("shutdown /s /t 0")
+    await ctx.send("Shutting down..")
+
+
+@bot.command()
+async def web(ctx, arg):
+    try:
+        webbrowser.get(browser_path).open(arg)
+        await ctx.send(f"Opened {arg} in your default browser.")
+    except Exception as e:
+        await ctx.send(f"Failed to open {arg}: {e}")
+
+
+@bot.command()
+async def restart(ctx, arg):
+    os.system("shutdown /r /t 1")
+    await ctx.send("Restarting..")
+
+@bot.command()
+async def keystroke(ctx, arg):
+
+    arg = arg.lower()
+
+    special_keys = {
+    "space": Key.space,
+    "shift": Key.shift,
+    "ctrl": Key.ctrl_l,
+    "enter": Key.enter,
+    "esc": Key.esc,
+    "backspace": Key.backspace,
+    "up": Key.up,
+    "down": Key.down,
+    "left": Key.left,
+    "right": Key.right,
+    "volumeup": Key.media_volume_up,
+    "volumedown": Key.media_volume_down,
+    
+    }
+
+    key = special_keys.get(arg.lower(), arg)
+
+    try:
+        keyboard.press(key)
+        keyboard.release(key)
+        await ctx.send(f"Pressed key: {arg}")
+    except ValueError as e:
+        await ctx.send(f"Invalid key: {arg}")
+
+
+
+bot.run(discord_token) # Add your token here
+
+# Developed by Faisal Abusharar.. More features soon :D
---
+++ compile.py
@@ -0,0 +1,76 @@
+import os
+import subprocess
+import sys
+
+package_name = "pyinstaller"
+
+user_profile_path = os.environ['USERPROFILE']
+username = os.path.basename(user_profile_path)
+
+# ------------- EDIT IF YOU KNOW WHAT YOU ARE DOING ---------------
+
+src = os.path.abspath(".\\dist\\CVDA.exe")
+dst = f"C:\\Users\\{username}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\"
+
+
+
+def tryInstallPyinstaller():
+    commands = [
+            [sys.executable, "-m", "pip", "install", "pyinstaller"],
+            ["pip", "install", "pyinstaller"],
+            ["pip3", "install", "pyinstaller"],
+        ]
+
+    for cmd in commands:
+        try:
+            print(f"Trying to install using: {' '.join(cmd)}")
+            result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            if result.returncode == 0:
+                print("PyInstaller installed successfully.")
+                return True
+        except Exception as e:
+                print(f"Failed with error: {e}")
+
+    print("All installation methods failed. Please install PyInstaller manually.")
+    return False
+
+def compile():
+    try: 
+        os.system("pyinstaller --onefile --noconsole CVDA.py")
+    except Exception as e:
+        print("Pyinstaller failed to compile, check if the script is avaliable in the same directory.")
+        print(e)
+        return False
+
+   
+    try:
+        os.system("taskkill /f /im  CVDA.exe")
+        subprocess.run(f'move /Y "{src}" "{dst}"', shell=True)
+        os.startfile(f"C:/Users/{username}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/CVDA.exe")
+    except:
+        print("Failed to move program to startup folder, may need to be done manually. Or dst variable may need to be edited.")
+        print("\n \n Move failed, starting application anyway.")
+        os.startfile(f"./dst/CVDA.exe")
+
+    return True
+
+
+try:
+    import PyInstaller
+    print("Pyinstaller is installed... compiling")
+    
+except:
+    print("Pyinstaller is not installed, downloading..")
+    installerResult = tryInstallPyinstaller()
+    if installerResult == False:
+        print("\n \n \n Program failed to install pyinstaller.. Ending.")
+
+
+
+
+compileResult = compile()
+if compileResult == True:
+    print(" \n \n \n Completed Compiler... Application has started, check to see if your discord bot is currently online.")
+else:
+    print("\n \n \n An error has occurred in the compiler.")
+
